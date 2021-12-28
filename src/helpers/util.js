0f559c1925195a66a00ddec452128071c386b241
benrobo <alumonabenaiah71@gmail.com>
2021-12-28 03:39:13 +0100

    🎉🎉 added notification lib and created some utility functions

---
+++ src/helpers/util.js
@@ -0,0 +1,185 @@
+import { Notyf } from "notyf";
+import "notyf/notyf.min.css";
+
+const notyf = new Notyf();
+
+export class Notification {
+    constructor(duration) {
+        this.duration = duration === undefined ? 3000 : duration;
+        this.position = {
+            x: "right",
+            y: "top",
+        };
+    }
+
+    success(msg, duration) {
+        return notyf.success({
+            duration: duration === undefined ? this.duration : duration,
+            message: msg === undefined || msg === null ? this.message : msg,
+            dismissible: true,
+            position: this.position,
+        });
+    }
+
+    error(msg, duration) {
+        return notyf.error({
+            duration: duration === undefined ? this.duration : duration,
+            message: msg === undefined || msg === null ? this.message : msg,
+            dismissible: true,
+            position: this.position,
+        });
+    }
+
+    dismissAll() {
+        return notyf.dismissAll();
+    }
+}
+
+export class Util {
+    Error(msg) {
+        return new Error(msg);
+    }
+
+    validatePhonenumber(phoneNumber) {
+        if (!phoneNumber) return false;
+        const regexp =
+            /^\+{0,2}([\-\. ])?(\(?\d{0,3}\))?([\-\. ])?\(?\d{0,3}\)?([\-\. ])?\d{3}([\-\. ])?\d{4}/;
+        return regexp.test(phoneNumber);
+    }
+
+    validateEmail(email) {
+        const tester =
+            /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+        if (!email) return false;
+
+        let emailParts = email.split("@");
+
+        if (emailParts.length !== 2) return false;
+
+        let account = emailParts[0];
+        let address = emailParts[1];
+
+        if (account.length > 64) return false;
+        else if (address.length > 255) return false;
+
+        let domainParts = address.split(".");
+        if (
+            domainParts.some(function (part) {
+                return part.length > 63;
+            })
+        )
+            return false;
+
+        if (!tester.test(email)) return false;
+
+        return true;
+    }
+
+    validateMatricNumber(matricNumber) {
+        let length = 6;
+        if (!matricNumber || matricNumber === undefined || matricNumber === "") {
+            return false;
+        }
+        // the matric number goes like this
+        // 18/0032
+        let check = matricNumber.trim().split("/").join("");
+
+        if (check.length > length || check.length !== length) {
+            return false;
+        }
+        return true;
+    }
+
+    validatePlaneNumber(plateNumber) {
+        let length = 6;
+        if (!plateNumber || plateNumber === undefined || plateNumber === "") {
+            return false;
+        }
+        // the matric number goes like this
+        // 18/0032
+        let check =
+            plateNumber.trim().split("").includes("-") === true
+                ? plateNumber.trim().split("-").join("")
+                : plateNumber.trim().split("").join("");
+
+        if (check.length > length || check.length !== length) {
+            return false;
+        }
+        return true;
+    }
+
+    randomImages(seeds) {
+        return `https://avatars.dicebear.com/api/initials/${seeds}.svg`;
+    }
+
+    sendJson(res, payload = { msg: "payload is empty" }, code = 401) {
+        if (!res) {
+            return this.Error("Rresponse object is required");
+        }
+        return res.status(code).json(payload);
+    }
+}
+
+export class Http {
+    async get(url, cb) {
+        if (
+            url === "" ||
+            url === undefined ||
+            typeof url === "function" ||
+            typeof url === Number
+        ) {
+            return cb(new Util().Error("Invalid url arguments passed"));
+        }
+        try {
+            const req = await fetch(url);
+            const res = await req.json();
+            return cb({ res, req });
+        } catch (e) {
+            return cb(e);
+        }
+    }
+
+    async post(
+        url,
+        payload,
+        headers={
+            "content-type": "application/json",
+        },
+        cb
+    ) {
+        if (
+            url === "" ||
+            url === undefined ||
+            typeof url === "function" ||
+            typeof url === Number
+        ) {
+            return cb(new Util.Error("Invalid url arguments passed"));
+        }
+
+        if (payload === "" || payload === undefined) {
+            return cb(new Util.Error("missing request body"));
+        }
+
+        try {
+            const req = await fetch(url, {
+                method: "post",
+                headers,
+                body: typeof payload === "string" ? payload : JSON.stringify(payload),
+            });
+            const res = await req.json();
+            console.log(typeof JSON.stringify(payload) === "string", res)
+            return cb({ res, req });
+        } catch (e) {
+            console.log(e)
+            // return cb(e);
+        }
+    }
+}
+
+class Error {
+    constructor(msg) {
+        this.msg = msg;
+        this.name = "Error";
+    }
+}
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,69 +1,176 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { ArrowLeftIcon } from '@heroicons/react/solid'
+import { ArrowLeftIcon } from "@heroicons/react/solid";
 
-import { SuccessBtn } from '../../helpers/buttons'
-import "./style.css"
-import { Link } from 'react-router-dom'
+import { SuccessBtn } from "../../helpers/buttons";
+import "./style.css";
+import { Link } from "react-router-dom";
 
+import { Notification, Util, Http } from "../../helpers/util";
+
+// instance
+const notif = new Notification(5000);
+const util = new Util();
+const http = new Http()
 
 function Signup() {
+  const [tab, setTab] = useState("student");
+  const [tabState, setTabState] = useState(true);
+  const [fullname, setFullname] = useState("");
+  const [mail, setMail] = useState("");
+  const [phonenumber, setPhonenumber] = useState("");
+  const [matricnumber, setMatricnumber] = useState("");
+  const [platenumber, setPlatenumber] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [tab, setTab] = useState("user")
-    const [active, setActive] = useState(false)
+  function handleClick() {
+    // validate input
+    if (fullname === "" || phonenumber === "" || password === "") {
+      return notif.error("all fields are required");
+    }
 
+    // validate email
+    if (!util.validateEmail(mail)) {
+      return notif.error("email is invalid");
+    }
 
+    // validate phonenumber
+    if(!util.validatePhonenumber(phonenumber)){
+        return notif.error("Invalid phonenumber: eg 07056448763");
+    }
 
-    return (
-        <>
-            <div className="auth-cont">
-                <div className="head mb-4">
-                    <Link to="/">
-                        <ArrowLeftIcon className="icon" />
-                    </Link>
-                    <h3>Create an Account</h3>
-                </div>
-                <div className="form-container mt-2">
-                    <div className="tab">
-                        <div className={active === true && tab === "user" ? "left active" : "left"} onClick={() => {
-                            setTab("user")
-                            setActive(!active)
-                        }}>User</div>
-                        <div className={active === true && tab === "driver" ? "right active" : "right"} onClick={() => {
-                            setTab("driver")
-                            setActive(!active)
-                        }}>Driver</div>
-                    </div>
-                    <br />
-                    {tab === "user" ?
-                        <div className="user-form form">
-                            <input type="text" placeholder="Full Name" className="input" autoFocus />
-                            <input type="tel" placeholder="Phonenumber" className="input"
-                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                                maxLength="11"
-                                required />
-                            <input type="email" placeholder="Email" className="input" />
-                            <input type="text" placeholder="Matric Number" className="input" />
-                            <input type="password" placeholder="Password" className="input" />
-                            <SuccessBtn text="Create Account" className={"btn-block"} />
-                            <small>have an account? <Link to="/signup">Sign In</Link></small>
-                        </div> :
-                        <div className="driver-form form">
-                            <input type="text" placeholder="Full Name" className="input" autoFocus />
-                            <input type="tel" placeholder="Phonenumber" className="input"
-                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                                maxLength="11"
-                                required />
-                            <input type="email" placeholder="Email" className="input" />
-                            <input type="number" placeholder="License Plate Number" className="input" />
-                            <input type="password" placeholder="Password" className="input" />
-                            <SuccessBtn text="Create Account" className={"btn-block"} />
-                            <small>have an account? <Link to="/signup">Sign In</Link></small>
-                        </div>}
-                </div>
+    // validate matric number
+    if (tab === "student" && !util.validateMatricNumber(matricnumber)) {
+      return notif.error("Invalid matric number: eg 18/0032");
+    }
+
+    // validate plate number
+    if (tab === "driver" && !util.validatePlaneNumber(platenumber)) {
+      return notif.error("license platenumber is invalid: eg AAA-553");
+    }
+
+    // else if every condition was met
+    // make request to server
+    const userData = {
+        name: fullname,
+        email: mail,
+        role: tab,
+        phoneNumber: phonenumber,
+        matricNumber: matricnumber,
+        password: password
+    }
+    const url = "http://localhost:5000/api/auth/register"
+    http.post(url, userData , (data)=>{
+        console.log(data)
+    })
+  }
+
+  return (
+    <>
+      <div className="auth-cont">
+        <div className="head mb-4">
+          <Link to="/">
+            <ArrowLeftIcon className="icon" />
+          </Link>
+          <h3>Create an Account</h3>
+        </div>
+        <div className="form-container mt-2">
+          <div className="tab">
+            <div className="box l">
+              <input
+                type="radio"
+                name="1"
+                value="student"
+                checked={tabState}
+                onClick={(e) => {
+                  setTab(e.target.value);
+                  setTabState(!tabState);
+                }}
+                className="radio"
+              />
+              <label htmlFor="">Students</label>
             </div>
-        </>
-    )
+            <div className="box r">
+              <input
+                type="radio"
+                name="1"
+                value="driver"
+                onClick={(e) => {
+                  setTab(e.target.value);
+                  setTabState(!tabState);
+                }}
+                className="radio"
+              />
+              <label htmlFor="">Driver</label>
+            </div>
+          </div>
+          <br />
+
+          <div className="user-form form">
+            <input
+              type="text"
+              placeholder="Full Name"
+              maxLength="20"
+              className="input"
+              autoFocus
+              onChange={(e) => {
+                setFullname(e.target.value);
+              }}
+            />
+            <input
+              type="tel"
+              placeholder="Phonenumber"
+              className="input"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              maxLength="11"
+              required
+              onChange={(e) => {
+                setPhonenumber(e.target.value);
+              }}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              onChange={(e) => {
+                setMail(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder={
+                tab === "student" ? "Matric Number" : "License Plate Number"
+              }
+              className="input"
+              maxLength="9"
+              onChange={(e) => {
+                tab === "student"
+                  ? setMatricnumber(e.target.value)
+                  : setPlatenumber(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              maxLength="20"
+              className="input mb-2"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <SuccessBtn
+              text="Create Account"
+              onClick={handleClick}
+              className={"btn-block"}
+            />
+            <small>
+              have an account? <Link to="/signup">Sign In</Link>
+            </small>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
