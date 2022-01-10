8ec6168d68c738408c6c8b69dfc0852860077489
alumonabenaiah@gmail.com <alumonabenaiah71@gmail.com>
2022-01-10 14:32:40 +0100

    added isLoggedIn functionality

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,56 +1,83 @@
-import React, { useState } from 'react'
-import { LightningBoltIcon, ShieldExclamationIcon, XIcon } from '@heroicons/react/solid'
+import React, { useState, useContext } from "react";
+import {
+  LightningBoltIcon,
+  ShieldExclamationIcon,
+  XIcon,
+} from "@heroicons/react/solid";
 
-import "./style.css"
-import { SuccessBtn } from '../../helpers/buttons'
-import image from "../../assets/img/car.png"
-import Modal from '../../components/Modal/Modal'
-import { Link } from 'react-router-dom'
+import "./style.css";
+import { SuccessBtn } from "../../helpers/buttons";
+import image from "../../assets/img/car.png";
+import Modal from "../../components/Modal/Modal";
+import { Link } from "react-router-dom";
+import DataContext from "../../context/DataContext";
+
+import { Util } from "../../helpers/util";
+
+const util = new Util();
 
 function Home() {
-    const [isOpen, setIsopen] = useState(false);
+  const { locData } = useContext(DataContext);
+  const [isOpen, setIsopen] = useState(false);
 
-    return (
-        <>
-            {isOpen && <Modal>
-                <div className="info-cont">
-                    <XIcon className="icon" onClick={() => setIsopen(!isOpen)} />
-                    <Link to="/signup" className="mt-2">
-                        <SuccessBtn text="Register" className="r-btn" />
-                    </Link>
-                    <Link to="/signin" className="mt-2">
-                        <SuccessBtn text="Sign In" className="s-btn" />
-                    </Link>
-                </div>
-            </Modal>}
-            <div className="main-cont">
-                <div className="left">
-                    {/* logo */}
+  return (
+    <>
+      {isOpen && (
+        <Modal>
+          <div className="info-cont">
+            <XIcon className="icon" onClick={() => setIsopen(!isOpen)} />
+            <Link to="/signup" className="mt-2">
+              <SuccessBtn text="Register" className="r-btn" />
+            </Link>
+            <Link to="/signin" className="mt-2">
+              <SuccessBtn text="Sign In" className="s-btn" />
+            </Link>
+          </div>
+        </Modal>
+      )}
+      <div className="main-cont">
+        <div className="left">
+          {/* logo */}
 
-                    {/* text */}
-                    <div className="text">
-                        <h3>Meet the perfect passenger.</h3>
-                        <br />
-                        {/* <img src={image} alt="" className="img-fluid car" /> */}
-                        <SuccessBtn onClick={() => setIsopen(!isOpen)} text="Get Started" className="mt-2" />
-                    </div>
-                </div>
-                <div className="right">
-                    <p>Features of Go-ride</p>
-                    <div className="features-box">
-                        <div className="box">
-                            <LightningBoltIcon className="icon" />
-                            <p>Reliable and Fast</p>
-                        </div>
-                        <div className="box">
-                            <ShieldExclamationIcon className="icon" />
-                            <p>Safe and Easy to use</p>
-                        </div>
-                    </div>
-                </div>
+          {/* text */}
+          <div className="text">
+            <h3>Meet the perfect passenger.</h3>
+            <br />
+            {/* <img src={image} alt="" className="img-fluid car" /> */}
+            <div className="action-btn">
+              <SuccessBtn
+                onClick={() => setIsopen(!isOpen)}
+                text="Get Started"
+                className="mt-2 mr-2"
+              />
+              {util.isLoggedIn() && (
+                <SuccessBtn
+                  onClick={() => {
+                    util.redirect(`/profile/${locData.id}`, 0);
+                  }}
+                  text="profile"
+                  className="mt-2 ml-2"
+                />
+              )}
             </div>
-        </>
-    )
+          </div>
+        </div>
+        <div className="right">
+          <p>Features of Go-ride</p>
+          <div className="features-box">
+            <div className="box">
+              <LightningBoltIcon className="icon" />
+              <p>Reliable and Fast</p>
+            </div>
+            <div className="box">
+              <ShieldExclamationIcon className="icon" />
+              <p>Safe and Easy to use</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/ride/Ride.js
+++ src/pages/ride/Ride.js
@@ -1,197 +1,217 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Head from "../../components/MainHead/Head";
-import { CheckCircleIcon, LocationMarkerIcon, XIcon } from "@heroicons/react/solid";
+import {
+  CheckCircleIcon,
+  LocationMarkerIcon,
+  XIcon,
+} from "@heroicons/react/solid";
 import "./style.css";
 import { SuccessBtn } from "../../helpers/buttons";
 import Modal from "../../components/Modal/Modal";
-import { ArrowLeftIcon } from '@heroicons/react/solid'
-import { Link } from 'react-router-dom'
+import { ArrowLeftIcon } from "@heroicons/react/solid";
+import { Link, useParams } from "react-router-dom";
 
-import cabImg from "../../assets/img/driver.png"
+import cabImg from "../../assets/img/driver.png";
 
 let locations = [
-    "Babcock Guest house",
-    "Main Gate",
-    "Winslow Hall",
-    "Neal Wilson Hall",
-    "Bethel Hall",
-    "Samuel Akande Hall",
-    "Nelson Mandela Hall",
-    "Queen Esther Hall",
-    "Nyberg Hall",
-    "FAD Hall",
-    "White Hall",
-    "Havilah Hall",
-    "Welch Hall",
-    "Crystal Hall",
-    "Platinum Hall",
-    "Babcock Stadium",
-    "Busa House",
-    "SAT",
-    "New Horizon",
-    "EAH",
-    "BBS",
-    "Babcock super store",
-    "Buth",
-    "Farm House",
-    "Amphitheater",
-    "Ogden Hall",
-    "Entrepreneurship center",
-    "Gideon Hall",
-    "Cafeteria",
-    "Adeleke hall",
-    "Marigold Hall",
+  "Babcock Guest house",
+  "Main Gate",
+  "Winslow Hall",
+  "Neal Wilson Hall",
+  "Bethel Hall",
+  "Samuel Akande Hall",
+  "Nelson Mandela Hall",
+  "Queen Esther Hall",
+  "Nyberg Hall",
+  "FAD Hall",
+  "White Hall",
+  "Havilah Hall",
+  "Welch Hall",
+  "Crystal Hall",
+  "Platinum Hall",
+  "Babcock Stadium",
+  "Busa House",
+  "SAT",
+  "New Horizon",
+  "EAH",
+  "BBS",
+  "Babcock super store",
+  "Buth",
+  "Farm House",
+  "Amphitheater",
+  "Ogden Hall",
+  "Entrepreneurship center",
+  "Gideon Hall",
+  "Cafeteria",
+  "Adeleke hall",
+  "Marigold Hall",
 ];
 
 function Ride() {
-    const [pickup, setPickup] = useState("");
-    const [drop, setDrop] = useState("");
-    const [suggestPickupHide, setPickupSuggestHide] = useState(false);
-    const [suggestDropHide, setDropSuggestHide] = useState(false);
-    const [suggestions, setSuggestions] = useState([]);
-    const [checkAction, setAction] = useState("");
-    const [getridedata, setGetrideData] = useState(null)
-    // const [showModal, setShowModal] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const [pickup, setPickup] = useState("");
+  const [drop, setDrop] = useState("");
+  const [suggestPickupHide, setPickupSuggestHide] = useState(false);
+  const [suggestDropHide, setDropSuggestHide] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  const [checkAction, setAction] = useState("");
+  const [getridedata, setGetrideData] = useState(null);
+  // const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  let params = useParams();
 
-    function handleGetRide() {
-        if (drop === "" || pickup === "") {
-            return alert("Fields cant be empty")
-        }
+  console.log(params);
 
-        let locationData = {
-            from: pickup,
-            to: drop
-        }
-        setGetrideData(locationData)
-        setLoading(true)
+  function handleGetRide() {
+    if (drop === "" || pickup === "") {
+      return alert("Fields cant be empty");
     }
 
-    function searchLocations(text) {
-        if (checkAction === "pickup") setPickupSuggestHide(true);
-        if (checkAction === "drop") setDropSuggestHide(true);
+    let locationData = {
+      from: pickup,
+      to: drop,
+    };
+    setGetrideData(locationData);
+    setLoading(true);
+  }
 
-        let matches = locations.filter((loc) => {
-            let regexp = new RegExp(text, "gi");
-            return loc.toLocaleUpperCase().match(regexp);
-        });
+  function searchLocations(text) {
+    if (checkAction === "pickup") setPickupSuggestHide(true);
+    if (checkAction === "drop") setDropSuggestHide(true);
 
-        setSuggestions([...matches]);
-    }
+    let matches = locations.filter((loc) => {
+      let regexp = new RegExp(text, "gi");
+      return loc.toLocaleUpperCase().match(regexp);
+    });
 
-    return (
-        <>
-            <Navbar />
-            <div className="ride-cont">
-                <div className="form">
-                    <div className="head mb-4">
-                        <Link to="/">
-                            <ArrowLeftIcon className="icon" />
-                        </Link>
-                        <h3>Get a Ride</h3>
-                    </div>
-                    <div className="pickup-box box">
-                        <CheckCircleIcon className="icon check" />
-                        <input
-                            type="text"
-                            placeholder="Pickup location"
-                            className="pickup inp"
-                            value={pickup}
-                            onChange={(e) => {
-                                setPickup(e.target.value);
-                                searchLocations(e.target.value);
-                                setAction("pickup");
-                            }}
-                        />
-                    </div>
-                    {suggestPickupHide && (
-                        <Suggestions
-                            locations={suggestions}
-                            select={setPickup}
-                            setSuggestions={setSuggestions}
-                            setAction={setAction}
-                            action={checkAction}
-                            setPickupSuggestHide={setPickupSuggestHide}
-                        />
-                    )}
-                    <div className="drop-box box">
-                        <LocationMarkerIcon className="icon location" />
-                        <input
-                            type="text"
-                            placeholder="Drop location"
-                            className="drop inp"
-                            value={drop}
-                            onChange={(e) => {
-                                setDrop(e.target.value);
-                                searchLocations(e.target.value);
-                                setAction("drop");
-                            }}
-                        />
-                    </div>
-                    {suggestDropHide && (
-                        <Suggestions
-                            locations={suggestions}
-                            select={setDrop}
-                            setSuggestions={setSuggestions}
-                            setAction={setAction}
-                            action={checkAction}
-                            setDropSuggestHide={setDropSuggestHide}
-                        />
-                    )}
-                    <br />
-                    {loading && <Modal>
-                        <div className="finding-driver">
-                            <div className="img-cont">
-                                <img src={cabImg} alt="" className="driver-lookup img-fluid" />
-                            </div>
-                            <XIcon className="icon" onClick={() => {
-                                setLoading(false)
-                                setGetrideData(null);
-                                alert("ride was cancel")
-                            }} />
-                        </div>
-                    </Modal>}
-                    <SuccessBtn text="Get a Ride" className="btn submit-btn" onClick={handleGetRide} />
+    setSuggestions([...matches]);
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="ride-cont">
+        <div className="form">
+          <div className="head mb-4">
+            <Link to="/">
+              <ArrowLeftIcon className="icon" />
+            </Link>
+            <h3>Get a Ride</h3>
+          </div>
+          <div className="pickup-box box">
+            <CheckCircleIcon className="icon check" />
+            <input
+              type="text"
+              placeholder="Pickup location"
+              className="pickup inp"
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value);
+                searchLocations(e.target.value);
+                setAction("pickup");
+              }}
+            />
+          </div>
+          {suggestPickupHide && (
+            <Suggestions
+              locations={suggestions}
+              select={setPickup}
+              setSuggestions={setSuggestions}
+              setAction={setAction}
+              action={checkAction}
+              setPickupSuggestHide={setPickupSuggestHide}
+            />
+          )}
+          <div className="drop-box box">
+            <LocationMarkerIcon className="icon location" />
+            <input
+              type="text"
+              placeholder="Drop location"
+              className="drop inp"
+              value={drop}
+              onChange={(e) => {
+                setDrop(e.target.value);
+                searchLocations(e.target.value);
+                setAction("drop");
+              }}
+            />
+          </div>
+          {suggestDropHide && (
+            <Suggestions
+              locations={suggestions}
+              select={setDrop}
+              setSuggestions={setSuggestions}
+              setAction={setAction}
+              action={checkAction}
+              setDropSuggestHide={setDropSuggestHide}
+            />
+          )}
+          <br />
+          {loading && (
+            <Modal>
+              <div className="finding-driver">
+                <div className="img-cont">
+                  <img
+                    src={cabImg}
+                    alt=""
+                    className="driver-lookup img-fluid"
+                  />
                 </div>
-            </div>
-        </>
-    );
+                <XIcon
+                  className="icon"
+                  onClick={() => {
+                    setLoading(false);
+                    setGetrideData(null);
+                    alert("ride was cancel");
+                  }}
+                />
+              </div>
+            </Modal>
+          )}
+          <SuccessBtn
+            text="Get a Ride"
+            className="btn submit-btn"
+            onClick={handleGetRide}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 function Suggestions({
-    locations,
-    select,
-    setSuggestions,
-    setAction,
-    setDropSuggestHide,
-    setPickupSuggestHide,
-    action,
+  locations,
+  select,
+  setSuggestions,
+  setAction,
+  setDropSuggestHide,
+  setPickupSuggestHide,
+  action,
 }) {
-    function handleSelect(text) {
-        select(text);
-    }
-    return (
-        <div className={"sugest-cont"}>
-            {locations.length > 0 &&
-                locations.map((text, i) => (
-                    <li
-                        key={i}
-                        onClick={() => {
-                            handleSelect(text);
-                            setSuggestions([]);
-                            action === "pickup"
-                                ? setPickupSuggestHide(false)
-                                : setDropSuggestHide(false);
-                            setAction("");
-                        }}
-                    >
-                        {text}
-                    </li>
-                ))}
-        </div>
-    );
+  function handleSelect(text) {
+    select(text);
+  }
+  return (
+    <div className={"sugest-cont"}>
+      {locations.length > 0 &&
+        locations.map((text, i) => (
+          <li
+            key={i}
+            onClick={() => {
+              handleSelect(text);
+              setSuggestions([]);
+              action === "pickup"
+                ? setPickupSuggestHide(false)
+                : setDropSuggestHide(false);
+              setAction("");
+            }}
+          >
+            {text}
+          </li>
+        ))}
+    </div>
+  );
 }
 
 export default Ride;
